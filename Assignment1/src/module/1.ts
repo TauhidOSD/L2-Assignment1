010745588b687ce190526dbb55c4de70141f00dd
Tauhid hossen <tauhidhossen04@gmail.com>
2025-05-08 00:33:56 +0600

    Solve 3rd problem

--- Assignment1/src/module/1.ts
+++ Assignment1/src/module/1.ts
@@ -25,5 +25,20 @@
     ]
 
     console.log(filterByRating(books));
+
+
+
+    //
+
+    function concatenateArrays<T>(...arrays: T[][]):T[]{
+        return arrays.reduce((acc, curr) => acc.concat(curr),[])
+            
+    }
+  
+    console.log(concatenateArrays(["a", "b"], ["c","d"]));
+    console.log(concatenateArrays([1,2], [3],[4],[5]));
+     
+
+
     
 }
